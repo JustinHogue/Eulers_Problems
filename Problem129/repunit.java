74d29cf93ccce5b422c6ad39d248870942cbc61c
JustinHogue <justin.hogue.2@ulaval.ca>
2021-09-04 16:39:59 -0400

    Added parallel threads

--- Problem129/repunit.java
+++ Problem129/repunit.java
@@ -1,64 +1,84 @@
-import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.*;
 
-public class repunit {
-    public static BigInteger R (int k){
-        return new BigInteger(new String(new char[k]).replace("\0", "1"));
+public class repunit extends Thread {
+    private static final List<Integer> list = IntStream.range(2, 1200000).boxed().collect(Collectors.toList());;
+    
+    public static String R(int k) {
+        return new String(new char[k]).replace("\0", "1");
     }
-    public static int A (int n) {
-        int i = 1;
-        while(true){
-            if (R(i).mod(BigInteger.valueOf(n)).equals(BigInteger.ZERO)) {
+
+    public static int A(int n) {
+        for (int i: list) {
+            if (longDivisionReminder(i, n) == 0) {
                 return i;
             }
-            i++;
         }
+        return 0;
+        //return list.stream().filter(i -> longDivisionReminder(i, n) == 0).findFirst().orElse(0);
     }
-    private static double log3(double logNumber) {
-        return Math.log(logNumber) / Math.log(3);
+
+    private static int findLengthOfInteger(int number) {
+        int length = 1;
+        if (number >= 100000000) {
+            length += 8;
+            number /= 100000000;
+        }
+        if (number >= 10000) {
+            length += 4;
+            number /= 10000;
+        }
+        if (number >= 100) {
+            length += 2;
+            number /= 100;
+        }
+        if (number >= 10) {
+            length += 1;
+        }
+        return length;
     }
+
+    public static int longDivisionReminder(int i, int divisor) {
+        int length = findLengthOfInteger(divisor);
+        int carry = (int) Math.pow(10, length) + Integer.parseInt("1".repeat(length));
+        while (length < i - 1) {
+            carry = (carry * 10 + 1) % divisor;
+            length++;
+        }
+        return carry;
+    }
+
     public static void main(String[] args) {
+        repunit thread = new repunit();
+        thread.start();
         int k = 0;
-        int n = 1000005;
-        while (k < 1000000){
-            if (n % 5 == 0){
-                n = n + 2;
+        int n = 999999;
+        while (k < 1000000) {
+            n = n + 2;
+            if (n % 5 == 0) {
                 continue;
-            } else if (log3(n) % 1 == 0) {
-                k = n;
-            } else if (log3(n/7) % 1 == 0) {
-                k = 2 * n/7;
-            } else if (log3(n/11) % 1 == 0) {
-                k = 2 * n/11;
-            } else if (log3(n/13) % 1 == 0) {
-                k = 2 * n/13;
-            } else if (log3(n/17) % 1 == 0) {
-                k = 16 * n/17;
-            } else if (log3(n/19) % 1 == 0) {
-                k = 2 * n/19;
-            } else if (log3(n/23) % 1 == 0) {
-                k = 22 * n/23;
-            } else if (log3(n/29) % 1 == 0) {
-                k = 28 * n/29;
-            } else if (log3(n/31) % 1 == 0) {
-                k = 5 * n/31;
-            } else if (log3(n/37) % 1 == 0) {
-                k = n/37;
-            } else if (log3(n/41) % 1 == 0) {
-                k = 5 * n/41;
-            } else if (log3(n/43) % 1 == 0) {
-                k = 7 * n/43;
-            } else if (log3(n/47) % 1 == 0) {
-                k = 46 * n/47;
-            } else if (log3(n/59) % 1 == 0) {
-                k = 58 * n/59;
-            } else if (log3(n/61) % 1 == 0) {
-                k = 60 * n/61;
-            } else {
+            } else if (n % 10 == 3 || n % 10 == 7) {
+                System.out.println("This is n:" + n);
                 k = A(n);
             }
-            System.out.println("This is n:" + n);
-            System.out.println("This is k:" + k);
-            n = n + 2;
         }
+        System.out.println("Le thread parent l'a trouvé! C'est k = " + k + " et n = " + n);
+        
+    }
+
+    public void run() {
+        int k = 0;
+        int n = 999999;
+        while (k < 1000000) {
+            n = n + 2;
+            if (n % 5 == 0) {
+                continue;
+            } else if (n % 10 == 1 || n % 10 == 9) {
+                System.out.println("This is n:" + n);
+                k = A(n);
+            }
+        }
+        System.out.println("Le thread enfant l'a trouvé! C'est k = " + k + " et n = " + n);
+        System.exit(1);
     }
 }
