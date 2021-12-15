70aa52d682687ec366b48a5f0203e74eb492f950
JustinHogue <justin.hogue.2@ulaval.ca>
2021-12-15 09:53:54 -0500

    Finished add in 201

--- Problem201/SubsetsWithAUniqueSum.java
+++ Problem201/SubsetsWithAUniqueSum.java
@@ -1,5 +1,14 @@
+import java.util.List;
+
 public class SubsetsWithAUniqueSum {
+    public static long add(List<Integer> subset) {
+        long sum = 0;
+        for(Integer currentValue : subset) {
+            sum += currentValue;
+        }
+        return sum;
+    }
     public static void main(String[] args) {
-        
+
     }
 }
