785cb4a1cc2bd3d9369960caa7b059aff16921c5
JustinHogue <justin.hogue.2@ulaval.ca>
2021-12-15 11:58:08 -0500

    Brute-Force Solution Problem 201

--- Problem201/SubsetsWithAUniqueSum.java
+++ Problem201/SubsetsWithAUniqueSum.java
@@ -1,14 +1,50 @@
-import java.util.List;
+import java.util.*;
 
 public class SubsetsWithAUniqueSum {
-    public static long add(List<Integer> subset) {
+    static int LENGTH_OF_SUBSET = 50;
+    public static long addAllElementsOf(ArrayList<Integer> subset) {
         long sum = 0;
-        for(Integer currentValue : subset) {
-            sum += currentValue;
-        }
+        sum = subset.parallelStream().map(currentValue -> (long) currentValue).reduce(sum, (accumulator, _item) -> accumulator + _item);
         return sum;
     }
+    public static ArrayList<ArrayList<Integer>> findAllSubsets (ArrayList<Integer> list, ArrayList<ArrayList<Integer>> answer){
+        if (list.size() == LENGTH_OF_SUBSET) {
+            if (!answer.contains(list))
+                answer.add(list);
+            return answer;
+        }
+        if (list.size() < LENGTH_OF_SUBSET) 
+            return new ArrayList<>();
+        for (Iterator<Integer> iterator = list.iterator(); iterator.hasNext(); ) {
+            iterator.next();
+            iterator.remove();
+            ArrayList<Integer> copiedList = new ArrayList<>();
+            list.stream().forEach(currentValue -> {
+                copiedList.add(currentValue);
+            });
+            System.out.println(answer.size());
+            findAllSubsets(copiedList, answer);
+        }
+        return answer;
+    }
     public static void main(String[] args) {
-
+        long indiceInMap = 1;
+        long sumOfTheSet = 0;
+        HashMap<Long, Long> uniqueSums = new HashMap<>();
+        ArrayList<ArrayList<Integer>> allSubsets = new ArrayList<>();
+        ArrayList<Integer> originalSet = new ArrayList<>(){{
+            for(int i = 1; i < 101; i++){
+                add(i*i);
+            }
+        }};
+        allSubsets = findAllSubsets(originalSet, allSubsets);
+        for (ArrayList<Integer> currentList : allSubsets) {
+            long sumOfTheSubset = addAllElementsOf(currentList);
+            if(!uniqueSums.containsValue(sumOfTheSubset)){
+                uniqueSums.put(indiceInMap++, sumOfTheSubset);
+                sumOfTheSet += sumOfTheSubset;
+            }
+        }
+        System.out.println(sumOfTheSet);
     }
 }
